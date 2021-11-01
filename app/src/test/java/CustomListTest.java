9786b332d3286731531be0cf95f4521715f1f4bd
joRusk <rusk@ualberta.ca>
2021-11-01 16:01:23 -0600

    added failing test

---
+++ app/src/test/java/CustomListTest.java
@@ -0,0 +1,24 @@
+import com.example.simpleparadox.listycity.City;
+import com.example.simpleparadox.listycity.CustomList;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+public class CustomListTest {
+
+    static CustomList list;
+
+    @Before
+    public void setup(){
+        list = new CustomList(null, new ArrayList<City>());
+    }
+
+    @Test
+    public void addCityTest(){
+        City c = new City("Edmonton","AB");
+        int size = list.getCount();
+        list.addCity(c);
+        assertEquals(size+1,list.getCount());
+    }
+}
